6333f1c973f854a9ce9faad6fbaeac99a7f94e5e
vivaan1709 <125991212+vivaan1709@users.noreply.github.com>
2023-06-27 20:14:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# C-43
+# C36_SpeedRacer_TeacherActivity
---
+++ js/Form.js
@@ -0,0 +1,54 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder","please enter your name")
+    this.playButton = createButton("PLAY");
+     this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h3");
+  }
+
+  setElementsPosition() {
+    this.input.position(windowWidth/2-100,windowHeight/2-100);
+    this.playButton.position(windowWidth/2-85,windowHeight/2-50);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+  setElementsStyle(){
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  handleMousePressed() {
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+
+      var message = `
+      WELCOME ${this.input.value()}
+      </br>Wait for another player to join...`;
+
+      this.greeting.html(message);
+      playerCount=playerCount+1;
+      player.name = this.input.value();
+      player.index = playerCount;
+      player.updateCount(playerCount);
+      console.log(player.index)
+      player.addPlayer();
+      player.getDistance();
+    });
+  }
+
+  hide(){
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+
+  }
+
+
+  display(){
+   this.setElementsPosition();
+   this.setElementsStyle();
+   this.handleMousePressed();
+  }
+}
